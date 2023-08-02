d83a3d6850f5a14af9e4de5a1c7305d4b36429a3
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-02 12:02:53 +0200

    Navigate Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate('/products')
+  }
     return (
         <>
             <div>
                 <h1>Home</h1>
                 <p>
                     Go To < Link to='/products'> the list of products.</Link></p>
+
+                    <p>
+                        <button onClick ={navigateHandler}>Navigate</button>
+                    </p>
             </div>
         </>
     )
