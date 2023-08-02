d0efbcad35f1ebcd71cacdacf9f8f523f484c1cd
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-02 11:00:17 +0200

    Adding Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
+
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    return <div>
-        <h1>Home</h1>
-    </div>;
+    return (
+        <>
+            <div>
+                <h1>Home</h1>
+                <p>
+                    Go To < Link to='/products'> the list of products.</Link></p>
+            </div>
+        </>
+    )
 }
 
 export default HomePage;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Products() {
   return (
     <div>
       <h1> Products</h1> 
-        </div>
+      <p>Go Back To < Link to='/'> Home.</Link></p>
+    </div>
+
   )
 }
