c94fe262283a343c5791d0a57beb954b9d05064d
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-02 11:55:27 +0200

    Adding css to Nav bar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/", 
     element: <RootLayout/>,
+    errorElement: <Error/>,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <Products/> },
-    
+      { path: "/error", element: <Error/> }, 
     ]
   },
+
  
 ]);
 function App() {
