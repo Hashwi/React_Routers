6cfd427fb08b8f5b6ea9c3f09f858f49ad1f8d54
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-02 10:49:31 +0200

    Adding products route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import Products from "./pages/Products";
 
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <Products/> },
 
-createBrowserRouter([
-  {path: '/', element: <HomePage/>},
-  {}
-
-]
-)
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
